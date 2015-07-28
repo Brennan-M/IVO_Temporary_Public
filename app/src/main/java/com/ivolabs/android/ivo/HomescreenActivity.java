c9b6657b80b465d32f1445671184d23ccaf7308e
Brennan McConnell <brmc3887@colorado.edu>
2015-07-28 00:43:31 -0700

    Added logout option via settings from the homescreen This allows us to make a create options menu for homescreen, I should disable the other ones.

--- app/src/main/java/com/ivolabs/android/ivo/HomescreenActivity.java
+++ app/src/main/java/com/ivolabs/android/ivo/HomescreenActivity.java
@@ -1,9 +1,11 @@
 package com.ivolabs.android.ivo;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 
 
 public class HomescreenActivity extends ActionBarActivity {
@@ -12,14 +14,6 @@ public class HomescreenActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
     }
 
     @Override
@@ -35,5 +29,19 @@ public class HomescreenActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+
+        menu.findItem(R.id.action_settings).setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                startActivity(new Intent(HomescreenActivity.this, SettingsActivity.class));
+                return true;
+            }
+        });
+        return true;
     }
 }
--- app/src/main/java/com/ivolabs/android/ivo/SettingsActivityFragment.java
+++ app/src/main/java/com/ivolabs/android/ivo/SettingsActivityFragment.java
@@ -1,10 +1,15 @@
 package com.ivolabs.android.ivo;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+import com.parse.ParseUser;
 
 
 /**
@@ -18,6 +23,21 @@ public class SettingsActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        Button logoutButton = (Button) view.findViewById(R.id.logoutUser_button);
+        logoutButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+
+                ParseUser.logOut();
+
+                Intent intent = new Intent(SettingsActivityFragment.this.getActivity(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
