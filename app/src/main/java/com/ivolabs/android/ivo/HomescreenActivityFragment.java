55be3a2862eece494c4baea78a30ebb70e59b949
Brennan McConnell <brmc3887@colorado.edu>
2015-07-28 21:28:22 -0700

    Finished setting up the various fragments and buttons. TODO: Make UI way nicer.

--- app/src/main/java/com/ivolabs/android/ivo/HomescreenActivityFragment.java
+++ app/src/main/java/com/ivolabs/android/ivo/HomescreenActivityFragment.java
@@ -1,10 +1,15 @@
 package com.ivolabs.android.ivo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 
 /**
@@ -18,6 +23,29 @@ public class HomescreenActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_screen, container, false);
+
+        final TextView welcomeTextView = (TextView) view.findViewById(R.id.welcomeUser);
+
+        Button startIvoPostActivityButton = (Button) view.findViewById(R.id.post_to_Ivo_button);
+        startIvoPostActivityButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomescreenActivityFragment.this.getActivity(), IvoPostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button startIvoFeedActivityButton = (Button) view.findViewById(R.id.get_Ivo_feed_button);
+        startIvoFeedActivityButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomescreenActivityFragment.this.getActivity(), IvoFeedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        welcomeTextView.setText("Welcome " + currentUser.getUsername());
+
+        return view;
     }
 }
